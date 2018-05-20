c6c4b8afdc1da1b7daa71c055ceb5bc34cf4618c
Jayden <wangzhenbang.1986@gmail.com>
2018-05-20 12:54:25 -0400

    git@github.com:wzbjmgs/emaily.git

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //root file
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+//https://mlab.com/databases/emaily-jayden-dev#users
+//go to this mongodb remote website to get the connection URL
+//do not copy the link directly here because everyone else
+//can get this link from git and then can access to our database
+mongoose.connect(keys.mongoURI);
 
 /*new a running express app. We can setup multiple express app
 majority applicaiton use single express app
